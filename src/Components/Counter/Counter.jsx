c53d11ca1f70caa5d07d134bcc00792af7f24afd
Marianne <m.salamanca21@live.ca>
2022-04-19 20:40:55 -0400

    change title and fix indentation in Counter component

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -8,10 +8,10 @@ export default function Counter(){
     return(
         <div className = 'Counter'>
             <h1>Counter Practice</h1>
-            <div>{counterData.counter}</div>
-            <hr />
-            <button onClick={()=>setCounterData({counter: counterData.counter -1})}>-</button>
-            <button onClick={()=>setCounterData({counter: counterData.counter +1})}>+</button>
+                <div>{counterData.counter}</div>
+                    <hr />
+                <button onClick={()=>setCounterData({counter: counterData.counter - 1})}>-</button>
+                <button onClick={()=>setCounterData({counter: counterData.counter + 1})}>+</button>
         </div>
     )
 }
